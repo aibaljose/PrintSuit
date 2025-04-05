d9ccfd4c94fd91b784ecb0b552731a463dbff5f0
aibaljose <aibaljosej@gmail.com>
2025-04-05 11:27:08 +0530

    changed package,vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
@@ -41,13 +40,13 @@ export default defineConfig({
   },
 
   optimizeDeps: {
-    include: ['pdfjs-dist'],
+    include: ['pdfjs-dist', 'react-hot-toast'],
   },
 
   build: {
     outDir: 'dist',
     commonjsOptions: {
-      include: [/pdfjs-dist/],
+      include: [/node_modules/, /pdfjs-dist/],
     },
   },
 });
